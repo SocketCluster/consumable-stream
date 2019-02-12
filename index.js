ffe42683286b9806eb2c5b8283cca6ce853d5e74
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-02-12 18:36:59 +0100

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 class AsyncIterableStream {
-  next(timeout) {
-    return this.createAsyncIterator(timeout).next();
+  async next(timeout) {
+    let asyncIterator = this.createAsyncIterator(timeout);
+    let result = await asyncIterator.next();
+    asyncIterator.return();
+    return result;
   }
 
   async once(timeout) {
